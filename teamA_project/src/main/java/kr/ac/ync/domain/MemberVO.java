020939ab3cb6d9f18d068831a5f07b071d3bd79d
tgkang <xorb6482@gmail.com>
2022-12-06 10:32:17 +0900

    Add Member[Register]

--- teamA_project/src/main/java/kr/ac/ync/domain/MemberVO.java
+++ teamA_project/src/main/java/kr/ac/ync/domain/MemberVO.java
@@ -4,16 +4,23 @@ import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class MemberVO {
 
+	private long memberIdx;
 	private String userid;
 	private String userpw;
+	private String nickName;
 	private String userName;
+	private String phoneNumber;
+	private String email;
+	private String address;
 	private boolean enabled;
-
+	private List<AuthVO> authList;
+	
 	private Date regDate;
 	private Date updateDate;
-	private List<AuthVO> authList;
 }
---
+++ teamA_project/src/main/java/kr/ac/ync/service/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package kr.ac.ync.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.ac.ync.domain.MemberVO;
+import kr.ac.ync.mapper.MemberMapper;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class MemberServiceImpl implements MemberService {
+	
+	MemberMapper mapper;
+
+	@Override
+	public void register(MemberVO memberVO) {
+		log.info("register ......" + memberVO);
+		mapper.register(memberVO);  
+	}
+}
